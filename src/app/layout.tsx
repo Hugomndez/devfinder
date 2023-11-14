e61c947460b8b4376146f2f22ac140f61e006876
Hugo Méndez <hello@hugomendez.dev>
2023-11-13 23:19:27 -0500

    feat: add space mono font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
+import { SpaceMono } from '@/fonts';
 import { getTheme } from '@/lib/getTheme';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './globals.css';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -12,11 +10,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
+    <html
+      lang='en'
+      dir='ltr'>
       <head>
         <script dangerouslySetInnerHTML={{ __html: getTheme }} />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={SpaceMono.className}>{children}</body>
     </html>
   );
 }
