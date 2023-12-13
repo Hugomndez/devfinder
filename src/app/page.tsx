d48d20e4ba751d3d157ebc82a89934f4288264de
Hugo Méndez <hello@hugomendez.dev>
2023-12-12 21:14:42 -0500

    refactor: home page code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,16 +7,14 @@ type Props = {
   searchParams: { [key: string]: string | string[] | undefined };
 };
 
-export async function generateMetadata({ searchParams }: Props): Promise<Metadata> {
-  const query = searchParams.q;
-
+export async function generateMetadata({ searchParams: { q } }: Props): Promise<Metadata> {
   return {
-    title: query ? `${query} | devFinder` : 'devFinder',
+    title: q ? `${q} | devFinder` : 'devFinder',
   };
 }
 
-export default async function Home({ searchParams }: Props) {
-  const query = typeof searchParams.q === 'string' ? searchParams.q : 'octocat';
+export default async function Home({ searchParams: { q } }: Props) {
+  const query = typeof q === 'string' ? q : 'octocat';
 
   return (
     <>
