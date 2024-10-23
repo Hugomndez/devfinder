08d7641510ab33e31d1608a35a28377652c8be8d
Hugo Méndez <hello@hugomendez.dev>
2024-10-22 20:35:01 -0500

    build: add types to eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,19 @@
 import { FlatCompat } from '@eslint/eslintrc';
 import js from '@eslint/js';
+
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: js.configs.recommended,
   allConfig: js.configs.all,
 });
 
+/** @type {import('eslint').Linter.Config[]} */
 const config = [...compat.extends('next/core-web-vitals'), { ignores: ['.next/*'] }];
 
 export default config;
