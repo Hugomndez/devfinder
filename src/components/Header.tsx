71236219f41ada98c995da3ddf9f05c6940a6a39
Hugo Méndez <hello@hugomendez.dev>
2024-11-07 18:49:40 -0500

    feat: after some querys by the user handle form reset on logo/app name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { revalidatePath } from 'next/cache';
 import dynamic from 'next/dynamic';
 import Link from 'next/link';
 import styles from './Header.module.css';
@@ -10,7 +11,9 @@ const ThemeToggle = dynamic(() => import('./theme-toggle'), {
 export default function Header() {
   return (
     <header className={styles.header}>
-      <Link href='/'>
+      <Link
+        href='/'
+        onClick={() => revalidatePath('/', 'page')}>
         <h1 aria-label='devFinder Home'>devFinder</h1>
       </Link>
       <ThemeToggle />
