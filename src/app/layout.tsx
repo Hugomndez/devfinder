8038b02340a0c63c17fd88d2e2951af6ca8e7c55
Hugo Méndez <hello@hugomendez.dev>
2024-11-08 11:54:57 -0500

    feat: add meta theme color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
 import ThemeScript from '@/components/theme-script';
 import { SpaceMono } from '@/fonts';
 import { APP_NAME } from '@/lib/constants';
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
   title: APP_NAME,
   description: 'Challenge by Frontend Mentor. Coded by @hugomndez.',
+};
+
+export const viewport: Viewport = {
+  themeColor: 'light dark',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
